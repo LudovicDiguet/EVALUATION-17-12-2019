20fc3a779ed6ce4a1e4eb5c658a348df94fd736f
LudovicDiguet <lludovic.diguet@ensta-bretagne.org>
2019-12-17 13:14:27 +0100

    first commit

---
+++ build/CMakeFiles/main_exe.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main_exe.dir/src/Cell.cpp.o"
+  "CMakeFiles/main_exe.dir/src/main.cpp.o"
+  "CMakeFiles/main_exe.dir/src/vibes.cpp.o"
+  "main_exe.pdb"
+  "main_exe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
